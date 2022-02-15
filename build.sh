95691a17d9408f42c9071f82a049137fecafb5c3
joakimmj <joakimmyrvoll@gmail.com>
2022-02-15 20:08:26 +0100

    refactor: rename `src/` folder to `dist/`

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-md-tangle configurations/bash.md -f -d src/
-md-tangle configurations/conkeror.md -f -d src/
-md-tangle configurations/conky.md -f -d src/
-md-tangle configurations/emacs.md -f -d src/
-md-tangle configurations/git.md -f -d src/
-md-tangle configurations/i3.md -f -d src/i3/
-md-tangle configurations/nano.md -f -d src/
-md-tangle configurations/xmodmap.md -f -d src/
+md-tangle configurations/bash.md -f -d dist/
+md-tangle configurations/conkeror.md -f -d dist/
+md-tangle configurations/conky.md -f -d dist/
+md-tangle configurations/emacs.md -f -d dist/
+md-tangle configurations/git.md -f -d dist/
+md-tangle configurations/i3.md -f -d dist/i3/
+md-tangle configurations/nano.md -f -d dist/
+md-tangle configurations/xmodmap.md -f -d dist/
