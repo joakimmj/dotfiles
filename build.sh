542840e872314d46f320675e3f300e9ef4fcbf38
joakimmj <joakimmyrvoll@gmail.com>
2024-04-11 23:01:43 +0200

    fix: add missing builds

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 md-tangle configurations/bash.md -f -d dist/
+md-tangle configurations/bin.md -f -d dist/bin/
 md-tangle configurations/conkeror.md -f -d dist/
 md-tangle configurations/conky.md -f -d dist/
 md-tangle configurations/emacs.md -f -d dist/
@@ -6,4 +7,5 @@ md-tangle configurations/git.md -f -d dist/
 md-tangle configurations/i3.md -f -d dist/i3/
 md-tangle configurations/ideavimrc.md -f -d dist/
 md-tangle configurations/nano.md -f -d dist/
+md-tangle configurations/tmux.md -f -d dist/tmux/
 md-tangle configurations/xmodmap.md -f -d dist/
