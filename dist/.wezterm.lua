af4eedaeea95b2140c84d08e5b4d00934caf99fa
Joakim Myrvoll <joakim.myrvoll@storebrand.no>
2024-10-23 13:36:51 +0200

    wezterm: update keymaps to not conflict with other shortcuts

--- dist/.wezterm.lua
+++ dist/.wezterm.lua
@@ -19,25 +19,37 @@ config.colors = {
     background = '#2E3434'
 }
 
+-- Window decorations
+config.enable_tab_bar = false
+config.window_decorations = 'RESIZE'
+
+wezterm.on('toggle-window-decorations', function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    if overrides.window_decorations == 'RESIZE' then
+        overrides.window_decorations = 'TITLE|RESIZE'
+    else
+        overrides.window_decorations = 'RESIZE'
+    end
+    window:set_config_overrides(overrides)
+end)
+
 -- Keymaps
 config.disable_default_key_bindings = true
 
 local act = wezterm.action
 config.keys = {
-    { key = 'c', mods = 'CTRL', action = act.CopyTo 'ClipboardAndPrimarySelection' },
-    { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
-    { key = 'n', mods = 'CTRL', action = act.SpawnWindow },
-    { key = '=', mods = 'CTRL', action = act.IncreaseFontSize },
-    { key = '-', mods = 'CTRL', action = act.DecreaseFontSize },
-    { key = '0', mods = 'CTRL', action = act.ResetFontSize },
-    { key = 'p', mods = 'CTRL', action = act.ActivateCommandPalette },
-    { key = 'r', mods = 'CTRL|SHIFT', action = act.ReloadConfiguration },
-    { key = ' ', mods = 'SHIFT|CTRL', action = act.QuickSelect },
+    { key = 'v',   mods = 'CTRL',                action = act.PasteFrom 'Clipboard' },
+    { key = 'c',   mods = 'CTRL|SHIFT',          action = act.CopyTo 'ClipboardAndPrimarySelection' },
+    { key = 'k',   mods = 'CTRL|SHIFT',          action = act.IncreaseFontSize },
+    { key = 'j',   mods = 'CTRL|SHIFT',          action = act.DecreaseFontSize },
+    { key = 'l',   mods = 'CTRL|SHIFT',          action = act.ResetFontSize },
+    { key = 'n',   mods = 'CTRL|SHIFT',          action = act.SpawnWindow },
+    { key = 'p',   mods = 'CTRL|SHIFT',          action = act.ActivateCommandPalette },
+    { key = 'r',   mods = 'CTRL|SHIFT',          action = act.ReloadConfiguration },
+    { key = 'w',   mods = 'CTRL|SHIFT',          action = act.EmitEvent 'toggle-window-decorations' },
+    { key = 'f',   mods = 'CTRL|SHIFT',          action = act.ToggleFullScreen },
+    { key = ' ',   mods = 'CTRL|SHIFT',          action = act.QuickSelect },
     { key = 'F11', action = act.ToggleFullScreen },
 }
-
--- Title bar
-config.enable_tab_bar = false
-config.window_decorations = 'TITLE|RESIZE'
 
 return config
