d94f1d9485739be282cfa693308ea88644163d0a
Joakim Myrvoll <joakim.myrvoll@storebrand.no>
2025-02-19 13:10:00 +0100

    feat(nvim): add mapping for showing diagnostic

--- dist/nvim/nvim/lua/my/plugins/lsp.lua
+++ dist/nvim/nvim/lua/my/plugins/lsp.lua
@@ -84,6 +84,9 @@ return {
 					--  For example, in C this would take you to the header.
 					map("gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
 
+					map("<leader>cd", function()
+						vim.diagnostic.open_float({ scope = 'line' })
+					end, "show [d]iagnostic")
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
 					--    See `:help CursorHold` for information about when this is executed
