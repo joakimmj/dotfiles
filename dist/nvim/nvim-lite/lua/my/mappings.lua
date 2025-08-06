e785b4376ea7637588d0a1bb539a2294906eb482
Joakim Myrvoll <joakim.myrvoll@storebrand.no>
2025-08-06 09:16:48 +0200

    feat(nvim): add keymaps

--- dist/nvim/nvim-lite/lua/my/mappings.lua
+++ dist/nvim/nvim-lite/lua/my/mappings.lua
@@ -5,9 +5,10 @@ vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "move line up" })
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "move line down" })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "move selection up" })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "move selection down" })
-vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
-vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
-vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
+vim.keymap.set("v", "<", "<gv", { desc = "indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "indent right and reselect" })
+vim.keymap.set("n", "J", "mzJ`z", { desc = "join lines and keep cursor position" })
+vim.keymap.set("n", "<leader>dw", "<cmd>set wrap!<CR>", { desc = "toggle [w]rap" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move cursor up" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move cursor down" })
 vim.keymap.set("n", "n", "nzzzv", { desc = "next search result (centered)" })
@@ -15,12 +16,18 @@ vim.keymap.set("n", "N", "Nzzzv", { desc = "previous search result (centered)" }
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "[y]ank to system clipboard" })
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "[Y]ank rest of line to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "[p]aste from system clipboard" })
-vim.keymap.set({"n", "v"}, "<C-p>", "\"0p", { desc = "paste from yank register" })
+vim.keymap.set({ "n", "v" }, "<C-p>", "\"0p", { desc = "paste from yank register" })
 vim.keymap.set(
 	"n",
 	"<leader>dr",
 	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
 	{ desc = "[r]eplace word" }
+)
+vim.keymap.set(
+	"v",
+	"<leader>dr",
+	[["hy:%s/<C-r>h//gI<left><left><left>]],
+	{ desc = "[r]eplace selected text" }
 )
 vim.keymap.set("n", "<leader>dd", vim.diagnostic.setloclist, { desc = "[d]iagnostic (location list)" })
 vim.keymap.set("n", "<leader>wd", vim.diagnostic.setqflist, { desc = "[d]iagnostic (quickfix list)" })
@@ -33,3 +40,4 @@ vim.keymap.set("n", "<up>", "<Nop>")
 vim.keymap.set("n", "<down>", "<Nop>")
 vim.keymap.set("n", "<leader>SS", function() vim.cmd([[mksession! .session.vim]]) end, { desc = "[S]ave" })
 vim.keymap.set("n", "<leader>SR", function() vim.cmd([[source .session.vim]]) end, { desc = "[R]estore" })
+vim.keymap.set("n", "<leader>we", ":Lex<CR>", { desc = "[e]xplorer" })
