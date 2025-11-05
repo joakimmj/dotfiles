83d600f23ff8a7b28e8fa7df4970149fe5d7ea9d
Joakim Myrvoll <joakim+github@myrvoll.dev>
2025-11-05 08:26:52 +0100

    refactor: update build paths

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
-md-tangle configurations/bash.md -f -d dist/
+md-tangle configurations/bash.md -f -d dist/bash/
 md-tangle configurations/bin.md -f -d dist/bin/
-md-tangle configurations/conkeror.md -f -d dist/
-md-tangle configurations/conky.md -f -d dist/
-md-tangle configurations/emacs.md -f -d dist/
+md-tangle configurations/conkeror.md -f -d dist/conkeror/
+md-tangle configurations/conky.md -f -d dist/conky/
+md-tangle configurations/emacs.md -f -d dist/emacs/
 md-tangle configurations/git.md -f -d dist/git/
 md-tangle configurations/i3.md -f -d dist/i3/
-md-tangle configurations/ideavimrc.md -f -d dist/
-md-tangle configurations/nano.md -f -d dist/
+md-tangle configurations/ideavimrc.md -f -d dist/ideavim/
+md-tangle configurations/nano.md -f -d dist/nano/
 md-tangle configurations/nvim.md -f -d dist/nvim/
 md-tangle configurations/tmux.md -f -d dist/tmux/
-md-tangle configurations/wezterm.md -f -d dist/
-md-tangle configurations/xmodmap.md -f -d dist/
+md-tangle configurations/wezterm.md -f -d dist/wezterm/
+md-tangle configurations/xmodmap.md -f -d dist/xmodmap/
 md-tangle configurations/yazi.md -f -d dist/yazi/
 
